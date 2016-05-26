d2cad62cb59d9b5750a77b96cb0afc46522bcbf3
tonychristopher90@gmail.com <Simon@123>
2016-05-26 18:19:37 +0530

    new

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new DateTime();
+           var sdsd= date.AddDays(2);
+        }
+    }
+}
